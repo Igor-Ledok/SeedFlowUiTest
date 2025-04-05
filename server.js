2ba73c214d95a4e7de4f3216f513faecc3e059d5
Игорь Льодок <igorledoksh@gmail.com>
2025-04-05 22:08:25 +0300

    second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Указываем Express использовать статические файлы из папки сборки Angular
-app.use(express.static(path.join(__dirname, '/dist/crowd-fund-platform/browser/index.html')));
+app.use(express.static(path.join(__dirname, '/dist/crowd-fund-platform/browser')));
 
 // Обрабатываем все остальные маршруты и возвращаем index.html
 app.get('*', (req, res) => {
